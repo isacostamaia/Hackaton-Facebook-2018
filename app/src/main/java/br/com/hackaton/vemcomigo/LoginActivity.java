033d217985977791ab6b206794cd843454161829
cynthiabaran <barancynthia@gmail.com>
2018-04-07 16:02:08 -0300

    add facebook login integration

--- app/src/main/java/br/com/hackaton/vemcomigo/LoginActivity.java
+++ app/src/main/java/br/com/hackaton/vemcomigo/LoginActivity.java
@@ -1,13 +1,73 @@
 package br.com.hackaton.vemcomigo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static br.com.hackaton.vemcomigo.R.*;
+
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.login_button)
+    LoginButton loginButton;
+
+    CallbackManager callbackManager;
+
+    private static final String EMAIL = "email";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(layout.activity_login);
+        ButterKnife.bind(this);
+
+        loginButton.setReadPermissions(Arrays.asList(EMAIL));
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton.registerCallback(callbackManager, getCallbackForLogin());
+        LoginManager.getInstance().registerCallback(callbackManager, getCallbackForLogin());
+
+    }
+
+    private FacebookCallback<LoginResult> getCallbackForLogin() {
+        final AppCompatActivity activity = this;
+
+        return new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Intent intent = new Intent(activity, MainActivity.class);
+                activity.startActivity(intent);
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        };
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (callbackManager != null) {
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }

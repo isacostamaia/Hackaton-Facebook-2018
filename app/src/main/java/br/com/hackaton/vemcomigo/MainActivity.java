4de1163b3e42054a26c3f08e18a19fb42760d21c
cynthiabaran <barancynthia@gmail.com>
2018-04-07 15:06:43 -0300

    add firebase code

--- app/src/main/java/br/com/hackaton/vemcomigo/MainActivity.java
+++ app/src/main/java/br/com/hackaton/vemcomigo/MainActivity.java
@@ -1,7 +1,17 @@
 package br.com.hackaton.vemcomigo;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+// Add a new document with a generated ID
+        db.collection("users")
+                .add(user);
+
     }
 }
+

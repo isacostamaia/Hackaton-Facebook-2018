596312180a72dc9d7ac24c212fc9c443915180d6
cynthiabaran <barancynthia@gmail.com>
2018-04-08 03:27:19 -0300

    deleting unused file

--- app/src/main/java/br/com/hackaton/vemcomigo/push/VemComigoInstanceIDService.java
+++
@@ -1,6 +0,0 @@
-package br.com.hackaton.vemcomigo.push;
-
-import com.google.firebase.iid.FirebaseInstanceIdService;
-
-public class VemComigoInstanceIDService extends FirebaseInstanceIdService {
-}
--- app/src/main/java/br/com/hackaton/vemcomigo/push/VemComigoMessagingService.java
+++
@@ -1,6 +0,0 @@
-package br.com.hackaton.vemcomigo.push;
-
-import com.google.firebase.messaging.FirebaseMessagingService;
-
-public class VemComigoMessagingService extends FirebaseMessagingService {
-}

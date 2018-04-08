fcff6c565fb278708eb0928c8177a0f915e92dc6
cynthiabaran <barancynthia@gmail.com>
2018-04-08 05:22:38 -0300

    implements ride communication

--- app/src/main/java/br/com/hackaton/vemcomigo/LoginActivity.java
+++ app/src/main/java/br/com/hackaton/vemcomigo/LoginActivity.java
@@ -1,19 +1,32 @@
 package br.com.hackaton.vemcomigo;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.Profile;
 import com.facebook.ProfileManager;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.gson.Gson;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,18 +40,15 @@ public class LoginActivity extends AppCompatActivity {
 
     CallbackManager callbackManager;
 
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_login);
         ButterKnife.bind(this);
 
-        boolean loggedIn = AccessToken.getCurrentAccessToken() != null;
-
-        if (loggedIn) {
-            Intent intent = new Intent(this, StartPinActivity.class);
-            startActivity(intent);
-        }
+        checkRide();
 
         loginButton.setReadPermissions(Arrays.asList("public_profile"));
 
@@ -48,6 +58,67 @@ public class LoginActivity extends AppCompatActivity {
         LoginManager.getInstance().registerCallback(callbackManager, getCallbackForLogin());
 
     }
+
+    private void checkRide() {
+
+        db.collection("rides")
+                .get()
+                .addOnCompleteListener(getOnCompleteListener());
+
+    }
+
+    public void handleLogin() {
+        boolean loggedIn = AccessToken.getCurrentAccessToken() != null;
+
+        boolean hasRide = UserUtils.getInstance().hasRide();
+
+        if (loggedIn) {
+
+            if (!hasRide) {
+                Intent intent = new Intent(this, StartPinActivity.class);
+                startActivity(intent);
+            } else {
+                Intent intent = new Intent(this, ConfirmationActivity.class);
+                startActivity(intent);
+            }
+        }
+    }
+
+    @NonNull
+    private OnCompleteListener<QuerySnapshot> getOnCompleteListener() {
+        return new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    List<Ride> rides = new ArrayList<>();
+                    Gson gson = new Gson();
+
+                    for (DocumentSnapshot document : task.getResult()) {
+
+                        Map<String, Object> data = document.getData();
+                        Coordinate startPoint = gson.fromJson((String) data.get("startPoint"), Coordinate.class);
+                        Coordinate endPoint = gson.fromJson((String) data.get("endPoint"), Coordinate.class);
+                        String company = (String) data.get("company");
+                        String loggedUserId = Profile.getCurrentProfile().getFirstName();
+                        String userId = (String) data.get("userId");
+
+                        if (loggedUserId.equals(userId) && !company.isEmpty()) {
+                            UserUtils.getInstance().setHasRide(true);
+                            handleLogin();
+                            Log.w("TEST", "user is" + company);
+                            return;
+                        }
+                    }
+                    handleLogin();
+
+                } else {
+                    handleLogin();
+                    Log.w("DB", "Error getting documents.", task.getException());
+                }
+            }
+        };
+    }
+
 
     private FacebookCallback<LoginResult> getCallbackForLogin() {
         final AppCompatActivity activity = this;
--- app/src/main/java/br/com/hackaton/vemcomigo/MainActivity.java
+++ app/src/main/java/br/com/hackaton/vemcomigo/MainActivity.java
@@ -31,6 +31,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -58,75 +59,75 @@ public class MainActivity extends AppCompatActivity implements
         // Called when the activity is first created.
         @Override
         public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-        // Instantiate the gesture detector with the
-        // application context and an implementation of
-        // GestureDetector.OnGestureListener
-        mDetector = new GestureDetectorCompat(this,this);
-        // Set the gesture detector as the double tap
-        // listener.
-        mDetector.setOnDoubleTapListener(this);
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            ButterKnife.bind(this);
+            // Instantiate the gesture detector with the
+            // application context and an implementation of
+            // GestureDetector.OnGestureListener
+            mDetector = new GestureDetectorCompat(this, this);
+            // Set the gesture detector as the double tap
+            // listener.
+            mDetector.setOnDoubleTapListener(this);
 
-        setTitle("Quem pode te acompanhar");
+            setTitle("Quem pode te acompanhar");
 
-        Intent intent = getIntent();
-        if (intent != null && intent.getStringExtra("currentRide") != null) {
-            String json = intent.getStringExtra("currentRide");
-            JsonParser parser = new JsonParser();
-            JsonObject rideJsonObject = parser.parse(json).getAsJsonObject();
-            this.currentRide = Ride.createFromJson(rideJsonObject);
-        }
+            Intent intent = getIntent();
+            if (intent != null && intent.getStringExtra("currentRide") != null) {
+                String json = intent.getStringExtra("currentRide");
+                JsonParser parser = new JsonParser();
+                JsonObject rideJsonObject = parser.parse(json).getAsJsonObject();
+                this.currentRide = Ride.createFromJson(rideJsonObject);
+            }
 
-        ridesList.setHasFixedSize(true);
-        mLayoutManager = new LinearLayoutManager(this);
-        ridesList.setLayoutManager(mLayoutManager);
+            ridesList.setHasFixedSize(true);
+            mLayoutManager = new LinearLayoutManager(this);
+            ridesList.setLayoutManager(mLayoutManager);
 
-        // specify an adapter (see also next example)
-        mAdapter = new RidesAdapter(this, rides, currentRide);
-        ridesList.setAdapter(mAdapter);
+            // specify an adapter (see also next example)
+            mAdapter = new RidesAdapter(this, rides, currentRide);
+            ridesList.setAdapter(mAdapter);
 
 
+            final Profile profile = Profile.getCurrentProfile();
+            if (profile != null) {
+                saudationTextView.setText("Olá, " + profile.getFirstName() + "! Escolha a sua companhia na lista abaixo");
+            }
 
-        final Profile profile = Profile.getCurrentProfile();
-        if (profile != null) {
-            saudationTextView.setText("Olá, " + profile.getFirstName()+"! Escolha a sua companhia na lista abaixo");
-        }
+            db.collection("rides")
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                List<Ride> rides = new ArrayList<>();
+                                Gson gson = new Gson();
 
-        db.collection("rides")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            List<Ride> rides = new ArrayList<>();
-                            Gson gson = new Gson();
+                                for (DocumentSnapshot document : task.getResult()) {
 
-                            for (DocumentSnapshot document : task.getResult()) {
+                                    Map<String, Object> data = document.getData();
+                                    Coordinate startPoint = gson.fromJson((String) data.get("startPoint"), Coordinate.class);
+                                    Coordinate endPoint = gson.fromJson((String) data.get("endPoint"), Coordinate.class);
+                                    String company = (String) data.get("company");
+                                    String loggedUserId = profile.getFirstName();
+                                    String userId = (String) data.get("userId");
 
-                                Map<String, Object> data = document.getData();
-                                Coordinate startPoint = gson.fromJson((String) data.get("startPoint"), Coordinate.class);
-                                Coordinate endPoint = gson.fromJson((String) data.get("endPoint"), Coordinate.class);
-                                String company = (String) data.get("company");
-                                String loggedUserId = profile.getFirstName();
-                                String userId = (String) data.get("userId");
+                                    if (!loggedUserId.equals(userId) && startPoint.getLatitude() != null && endPoint.getLatitude() != null) {
+                                        rides.add(new Ride(userId, startPoint, endPoint, company));
+                                    }
 
-                                if (!loggedUserId.equals(userId) && startPoint.getLatitude() != null && endPoint.getLatitude() != null) {
-                                    rides.add(new Ride(userId, startPoint, endPoint, company));
                                 }
 
+                                mAdapter.setRides(rides);
+                                mAdapter.notifyDataSetChanged();
+                            } else {
+                                Log.w("DB", "Error getting documents.", task.getException());
                             }
-
-                            mAdapter.setRides(rides);
-                            mAdapter.notifyDataSetChanged();
-                        } else {
-                            Log.w("DB", "Error getting documents.", task.getException());
                         }
-            }
-        });
+                    });
 
         }
+
 
         @Override
         public boolean onSingleTapConfirmed(MotionEvent e) {
@@ -135,6 +136,7 @@ public class MainActivity extends AppCompatActivity implements
 
         @Override
         public boolean onDoubleTap(MotionEvent e) {
+
             return false;
         }
 
@@ -145,7 +147,6 @@ public class MainActivity extends AppCompatActivity implements
 
         @Override
         public boolean onDown(MotionEvent e) {
-
             return false;
         }
 
